278e9f331e9bde631f59ace4741e7a0218a6f9cc
hhl <2498393441@qq.com>
2020-07-12 22:53:48 +0800

    查询新增

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="app">
     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <el-menu-item index="/stocks">股票列表</el-menu-item>
+      <el-submenu index="1">
+        <template slot="title">股票</template>
+        <el-menu-item index="/stocks">股票列表</el-menu-item>
+        <el-menu-item index="/query">查询</el-menu-item>
+      </el-submenu>
       <el-menu-item index="/shares">分红</el-menu-item>
-      <!--      <el-submenu index="2">-->
-      <!--        <template slot="title">分红</template>-->
-      <!--        <el-menu-item index="/shares">近期分红</el-menu-item>-->
-      <!--        <el-menu-item index="/query">查询</el-menu-item>-->
-      <!--      </el-submenu>-->
     </el-menu>
     <keep-alive>
       <router-view></router-view>
--- src/views/Stocks.vue
+++ src/views/Stocks.vue
@@ -22,5 +22,16 @@
         this.$refs.stock_list.loading = false
       })
     },
+    activated() {
+      // 当查询条件不为空时
+      if(this.$route.query.years && this.$route.query.dv_ratio){
+        this.$refs.stock_list.loading = true
+        this.stocks = []
+        getStocks({years: this.$route.query.years, dv_ratio:this.$route.query.dv_ratio}).then(res=>{
+          this.stocks = res.data['stocks']
+          this.$refs.stock_list.loading = false
+        })
+      }
+    }
   }
 </script>
