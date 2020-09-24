2baf5de184715b9fdbdd04e56d3e2ce05677e9aa
TheCodeIsWorld <35627468+TheCodeIsWorld@users.noreply.github.com>
2020-09-24 15:18:34 +0800

    Delete collections.js

--- src/network/collections.js
+++
@@ -1,9 +0,0 @@
-import {request} from "./request";
-
-export function getCollections(data) {
-    // return request({
-    //     url:'/get_stocks',
-    //     method: 'POST',
-    //     data: data,
-    // })
-}
